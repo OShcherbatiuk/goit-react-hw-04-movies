8490117f3b0a0f6747a6865b6f042a07f25dc802
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2021-05-08 23:36:30 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { NavLink, Route, Switch } from 'react-router-dom';
-
 import HomePage from './views/HomePage';
 import MoviesPage from './views/MoviesPage';
 import MovieDetailsPage from './views/MovieDetailsPage';
 import Cast from './views/Cast';
 import Reviews from './views/Reviews';
 import NotFoundView from './views/NotFoundView';
+import { Route, Switch } from 'react-router';
+import { NavLink } from 'react-router-dom';
 
 const App = () => (
   <>
@@ -28,10 +28,10 @@ const App = () => (
     </ul>
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/movies" component={MoviesPage} />
-      <Route path="/movies/:movieId" component={MovieDetailsPage} />
-      <Route path="/movies/:movieId/cast" component={Cast} />
-      <Route path="/movies/:movieId/reviews" component={Reviews} />
+      <Route exact path="/movies" component={MoviesPage} />
+      <Route exact path="/movies/:movieId" component={MovieDetailsPage} />
+      <Route exact path="/movies/:movieId/cast" component={Cast} />
+      <Route exact path="/movies/:movieId/reviews" component={Reviews} />
       <Route component={NotFoundView} />
     </Switch>
   </>
--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -1,5 +1,35 @@
-const HomePage = () => {
-  return <h1>HomePage</h1>;
-};
+import { Component } from 'react';
+import Axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class HomePage extends Component {
+  state = {
+    movie: [],
+  };
+  async componentDidMount() {
+    const response = await Axios.get(
+      'https://api.themoviedb.org/3/trending/all/day?api_key=455a0ddf1ae97a91f0c666d83d1a7d1f',
+    );
+
+    this.setState({ movie: response.data.results });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Pop films</h1>
+        <ul>
+          {this.state.movie.map(movie => (
+            <li key={movie.id}>
+              <Link to={`${this.props.match.url}/${movie.id}`}>
+                {movie.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default HomePage;
