3ddf3d38a1c659291dccc9b4b5e0afc246d7f7f7
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2021-05-09 15:25:19 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import HomePage from './views/HomePage';
 import MoviesPage from './views/MoviesPage';
 import MovieDetailsPage from './views/MovieDetailsPage';
-import Cast from './components/Cast';
-import Reviews from './components/Reviews';
 import NotFoundView from './views/NotFoundView';
 import { Route, Switch } from 'react-router';
 import { NavLink } from 'react-router-dom';
@@ -24,8 +22,6 @@ const App = () => (
       <Route exact path="/" component={HomePage} />
       <Route exact path="/movies" component={MoviesPage} />
       <Route exact path="/movies/:movieId" component={MovieDetailsPage} />
-      <Route exact path="/movies/:movieId/cast" component={Cast} />
-      <Route exact path="/movies/:movieId/reviews" component={Reviews} />
       <Route component={NotFoundView} />
     </Switch>
   </>
--- src/views/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/views/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,11 +1,59 @@
 import { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
+import Axios from 'axios';
+import Cast from '../../components/Cast';
+import Reviews from '../../components/Reviews';
 
 class MovieDetailsPage extends Component {
-  state = {};
+  state = {
+    movie: [],
+    genres: [],
+  };
+
+  async componentDidMount() {
+    const KEY = '455a0ddf1ae97a91f0c666d83d1a7d1f';
+    const searchQuery = `https://api.themoviedb.org/3/movie/${this.props.match.params.movieId}?api_key=${KEY}&language=en-US`;
+    const response = await Axios.get(searchQuery);
+
+    this.setState({ movie: response.data, genres: response.data.genres });
+    console.log(this.state);
+  }
+
   render() {
+    const { match } = this.props;
     return (
       <div>
         <button type="button">Go back</button>
+        <div>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${this.state.movie.poster_path}`}
+            alt=""
+          />
+          <h1>
+            {this.state.movie.title}(
+            {new Date(this.state.movie.release_date).getFullYear()})
+          </h1>
+          <p>User score: {this.state.movie.vote_average}</p>
+          <h2>Overview</h2>
+          <p>{this.state.movie.overview}</p>
+          <h3>Genres</h3>
+          <ul>
+            {this.state.genres.map(genre => (
+              <li key={genre.id}>{genre.name}</li>
+            ))}
+          </ul>
+        </div>
+        <p>Aditional infomation</p>
+        <ul>
+          <li>
+            <NavLink to={`${match.url}/cast`}>Cast</NavLink>
+          </li>
+          <li>
+            <NavLink to={`${match.url}/reviews`}>Reviews</NavLink>
+          </li>
+        </ul>
+        <Route exact path={`${match.url}/cast`} component={Cast} />
+        <Route exact path={`${match.url}/reviews`} component={Reviews} />
       </div>
     );
   }
