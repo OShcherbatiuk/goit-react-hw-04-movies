323341bc25fe879bd0ac08234108db736f961872
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2021-05-13 20:31:09 +0300

    upd

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
+import api from './api';
+const { API_KEY } = api;
+
 const KEY = '455a0ddf1ae97a91f0c666d83d1a7d1f';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
 export default class ApiService {
   async getTradingMovies() {
-    const response = await axios.get(`/trending/all/week?api_key=${KEY}`);
+    const response = await axios.get(`/trending/all/week?api_key=${API_KEY}`);
     return response.data.results;
   }
 
@@ -15,18 +18,18 @@ export default class ApiService {
     return response.data.results;
   }
   async getMovieById(movieId) {
-    const response = await axios.get(`/movie/${movieId}?api_key=${KEY}`);
+    const response = await axios.get(`/movie/${movieId}?api_key=${API_KEY}`);
     return response.data;
   }
   async getMovieCredits(movieId) {
     const response = await axios.get(
-      `/movie/${movieId}/credits?api_key=${KEY}`,
+      `/movie/${movieId}/credits?api_key=${API_KEY}`,
     );
     return response.data.cast;
   }
   async getMovieReviews(movieId) {
     const response = await axios.get(
-      `/movie/${movieId}/reviews?api_key=${KEY}`,
+      `/movie/${movieId}/reviews?api_key=${API_KEY}`,
     );
     return response.data.results;
   }
---
+++ src/services/api.js
@@ -0,0 +1,5 @@
+const api = {
+  API_KEY: '455a0ddf1ae97a91f0c666d83d1a7d1f',
+};
+
+export default api;
